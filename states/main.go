5727444c7c8f8f75367c75431da65cea5fa5daae
Harrison Borges <Hbbb@users.noreply.github.com>
2020-11-25 21:23:18 -0800

    Rerrange and add sentry

--- states/main.go
+++ states/main.go
@@ -7,55 +7,49 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"sync"
+	"time"
 
 	"cloud.google.com/go/firestore"
+	"github.com/getsentry/sentry-go"
 )
 
-func ImportDaily() {
-
+// Row is a row
+type Row struct {
+	Date   string
+	County string
+	State  string
+	Fips   string
+	Cases  string
+	Deaths string
 }
 
-func getPreviousOffset(ctx context.Context, db *firestore.Client, scope string, url string) int64 {
-	// fetch previous stopping point
-	doc, err := db.Collection("offsets").Doc(scope).Get(ctx)
-	if err != nil {
-		panic(err)
-	}
-
-	o, err := doc.DataAt("offset")
-	if err != nil {
-		panic(err)
-	}
-
-	offset := o.(int64)
-
-	return offset
+// ImportStatesHistorical Cloud Function
+func ImportStatesHistorical(ctx context.Context, message interface{}) error {
+	return importHistorical("state", "states-historical", "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv", processStateRow)
 }
 
-func getCurrentOffset(ctx context.Context, url string) int {
-	h, err := http.Head(url)
-	if err != nil {
-		panic(err)
-	}
-	defer h.Body.Close()
-
-	clen := h.Header["Content-Length"][0]
-	len, err := strconv.Atoi(clen)
-	if err != nil {
-		panic(err)
-	}
-
-	return len
+// ImportCountiesHistorical Cloud Function
+func ImportCountiesHistorical(ctx context.Context, message interface{}) error {
+	return importHistorical("county", "counties-historical", "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv", processCountyRow)
 }
 
-func ImportHistorical(scope string, collectionName string, url string, processRow func([]string) Row) error {
+func importHistorical(scope string, collectionName string, url string, processRow func([]string) Row) error {
 	ctx := context.Background()
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: os.Getenv("SENTRY_DSN"),
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer sentry.Flush(2 * time.Second)
 
 	db, err := createDBClient(ctx)
 	if err != nil {
-		return err
+		sentry.CaptureException(err)
+		panic(err)
 	}
 
 	previousOffset := getPreviousOffset(ctx, db, scope, url)
@@ -66,6 +60,7 @@ func ImportHistorical(scope string, collectionName string, url string, processRo
 
 	resp, err := client.Do(req)
 	if err != nil {
+		sentry.CaptureException(err)
 		return err
 	}
 	defer resp.Body.Close()
@@ -89,6 +84,7 @@ func ImportHistorical(scope string, collectionName string, url string, processRo
 		}
 
 		if err != nil {
+			sentry.CaptureException(err)
 			return err
 		}
 
@@ -105,6 +101,7 @@ func ImportHistorical(scope string, collectionName string, url string, processRo
 			// log.Println("processing row", row.Fips+"_"+row.Date)
 			collection := db.Collection(collectionName)
 			if _, err := collection.Doc(row.Fips+"_"+row.Date).Set(ctx, row); err != nil {
+				sentry.CaptureException(err)
 				panic(err)
 			}
 			w.Done()
@@ -119,6 +116,7 @@ func ImportHistorical(scope string, collectionName string, url string, processRo
 	}})
 
 	if err != nil {
+		sentry.CaptureException(err)
 		return nil
 	}
 
@@ -126,23 +124,41 @@ func ImportHistorical(scope string, collectionName string, url string, processRo
 	return nil
 }
 
-// Cloud Function
-func ImportStatesHistorical(ctx context.Context, message interface{}) error {
-	return ImportHistorical("state", "states-historical", "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv", processStateRow)
+func getPreviousOffset(ctx context.Context, db *firestore.Client, scope string, url string, sentry ) int64 {
+	// fetch previous stopping point
+	doc, err := db.Collection("offsets").Doc(scope).Get(ctx)
+	if err != nil {
+		sentry.CaptureException(err)
+		panic(err)
+	}
+
+	o, err := doc.DataAt("offset")
+	if err != nil {
+		sentry.CaptureException(err)
+		panic(err)
+	}
+
+	offset := o.(int64)
+
+	return offset
 }
 
-// Cloud Function
-func ImportCountiesHistorical(ctx context.Context, message interface{}) error {
-	return ImportHistorical("county", "counties-historical", "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv", processCountyRow)
-}
+func getCurrentOffset(ctx context.Context, url string) int {
+	h, err := http.Head(url)
+	if err != nil {
+		sentry.CaptureException(err)
+		panic(err)
+	}
+	defer h.Body.Close()
 
-type Row struct {
-	Date   string
-	County string
-	State  string
-	Fips   string
-	Cases  string
-	Deaths string
+	clen := h.Header["Content-Length"][0]
+	len, err := strconv.Atoi(clen)
+	if err != nil {
+		sentry.CaptureException(err)
+		panic(err)
+	}
+
+	return len
 }
 
 func processStateRow(row []string) Row {
@@ -171,6 +187,7 @@ func createDBClient(ctx context.Context) (*firestore.Client, error) {
 
 	client, err := firestore.NewClient(ctx, projectID)
 	if err != nil {
+		sentry.CaptureException(err)
 		return nil, err
 	}
 
