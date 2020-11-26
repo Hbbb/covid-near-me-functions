6110b56249bf773940759f895c14cedb794e57b4
Harrison Borges <Hbbb@users.noreply.github.com>
2020-11-25 23:52:42 -0800

    logs in test file

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ func main() {
 	}
 	// t := time.Now().In(loc)
 	// log.Println(t.Zone())
-	fmt.Println(time.Now().Format("2006-01-02 15:04 z-07:00"))
-	fmt.Println(time.Now().In(loc).Format("2006-01-02 15:04 z-07:00"))
-	fmt.Println(time.Now().UTC().Format("2006-01-02 15:04 z-07:00"))
+	fmt.Println("now", time.Now().Format("2006-01-02 15:04 z-07:00"))
+	fmt.Println("zone", time.Now().In(loc).Format("2006-01-02 15:04 z-07:00"))
+	fmt.Println("utc", time.Now().UTC().Format("2006-01-02 15:04 z-07:00"))
 
 }
