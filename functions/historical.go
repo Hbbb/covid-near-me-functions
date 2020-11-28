77e8d1e4562856efd61a82f5461f1b22b99746af
Harrison Borges <Hbbb@users.noreply.github.com>
2020-11-28 12:57:58 -0800

    Literally handle errors

--- functions/historical.go
+++ functions/historical.go
@@ -17,31 +17,54 @@ import (
 
 // ImportStatesHistorical Cloud Function
 func ImportStatesHistorical(ctx context.Context, message interface{}) error {
-	return importHistorical("state", "states-historical", "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv", processHistoricalStateRow)
-}
-
-// ImportCountiesHistorical Cloud Function
-func ImportCountiesHistorical(ctx context.Context, message interface{}) error {
-	return importHistorical("county", "counties-historical", "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv", processHistoricalCountyRow)
-}
-
-func importHistorical(scope string, collectionName string, url string, processRow func([]string) historicalRow) error {
-	ctx := context.Background()
 	err := sentry.Init(sentry.ClientOptions{
 		Dsn: os.Getenv("SENTRY_DSN"),
 	})
 	if err != nil {
 		panic(err)
 	}
+	defer sentry.Recover()
 	defer sentry.Flush(2 * time.Second)
 
-	db, err := createDBClient(ctx)
+	err = importHistorical("state", "states-historical", "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv", processHistoricalStateRow)
 	if err != nil {
-		sentry.CaptureException(err)
 		panic(err)
 	}
 
-	previousOffset := getPreviousOffset(ctx, db, scope, url)
+	return nil
+}
+
+// ImportCountiesHistorical Cloud Function
+func ImportCountiesHistorical(ctx context.Context, message interface{}) error {
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: os.Getenv("SENTRY_DSN"),
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer sentry.Recover()
+	defer sentry.Flush(2 * time.Second)
+
+	err = importHistorical("county", "counties-historical", "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv", processHistoricalCountyRow)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func importHistorical(scope string, collectionName string, url string, processRow func([]string) (historicalRow, error)) error {
+	ctx := context.Background()
+
+	db, err := createDBClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	previousOffset, err := getPreviousOffset(ctx, db, scope, url)
+	if err != nil {
+		return fmt.Errorf("failed to fetch previous offset for %s: %e", scope, err)
+	}
 
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", url, nil)
@@ -49,14 +72,12 @@ func importHistorical(scope string, collectionName string, url string, processRo
 
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("http request failed (likely a connectivity problem)", collectionName)
-		sentry.CaptureException(err)
 		return err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode == 416 {
-		fmt.Println("No new data in file", collectionName)
+		fmt.Printf("No new data in %s spreadsheet\n", scope)
 		return nil
 	}
 
@@ -64,19 +85,14 @@ func importHistorical(scope string, collectionName string, url string, processRo
 	reader.Comma = ','
 
 	firstLine := true
-
 	wg := sync.WaitGroup{}
-
-	batch := db.Batch()
 	for {
 		data, err := reader.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
-			fmt.Println("error reading CSV line")
-			sentry.CaptureException(err)
-			return err
+			return fmt.Errorf("error reading CSV line: %e", err)
 		}
 
 		if firstLine && previousOffset == 0 {
@@ -84,93 +100,114 @@ func importHistorical(scope string, collectionName string, url string, processRo
 			continue
 		}
 
+		row, err := processRow(data)
+		if err != nil {
+			return fmt.Errorf("failed to process row: %e", err)
+		}
+
 		wg.Add(1)
-		go func(d []string, w *sync.WaitGroup) {
-			row := processRow(d)
+		go func(hub *sentry.Hub) {
+			hub.ConfigureScope(func(scope *sentry.Scope) {
+				scope.SetTag("fips", row.Fips)
+				scope.SetTag("state", row.State)
+				scope.SetTag("county", row.County)
+			})
 
 			if row.County == "New York City" {
 				row.Fips = "NYC"
 			}
 
 			doc := db.Collection(collectionName).Doc(row.Fips + "_" + row.Date)
+			if _, err := doc.Set(ctx, map[string]interface{}{
+				"Date":   row.Date,
+				"County": row.County,
+				"State":  row.State,
+				"Fips":   row.Fips,
+				"Cases":  row.Cases,
+				"Deaths": row.Deaths,
+			}, firestore.MergeAll); err != nil {
+				fmt.Println("failed to store historical cases for", row.Fips, row.State, row.County)
+				hub.CaptureException(err)
+			}
 
-			batch.Set(doc, row)
-			w.Done()
-		}(data, &wg)
+			wg.Done()
+		}(sentry.CurrentHub().Clone())
 	}
 
-	length := getCurrentOffset(ctx, url)
+	// We update the offset to be the size of the full file
+	// Next time we import, when the file grows, we'll pull from this offset
+	length, err := getNextOffset(ctx, url)
+	if err != nil {
+		return fmt.Errorf("failed to fetch next offset: %e", err)
+	}
 
 	_, err = db.Collection("offsets").Doc(scope).Update(ctx, []firestore.Update{{
 		Path:  "offset",
 		Value: length,
 	}})
 	if err != nil {
-		fmt.Println("failed to update offset", scope)
-		sentry.CaptureException(err)
-		return nil
+		return fmt.Errorf("failed to update next offset: %e", err)
 	}
 
 	wg.Wait()
-
-	if _, err := batch.Commit(ctx); err != nil {
-		sentry.CaptureException(err)
-		fmt.Println("failed to write all documents")
-	}
 	return nil
 }
 
-func getPreviousOffset(ctx context.Context, db *firestore.Client, scope string, url string) int64 {
-	// fetch previous stopping point
+// fetch previous stopping point (file's Content-Length) from last time we imported
+func getPreviousOffset(ctx context.Context, db *firestore.Client, scope string, url string) (int64, error) {
 	doc, err := db.Collection("offsets").Doc(scope).Get(ctx)
 	if err != nil {
-		fmt.Println("failed to fetch offsets document")
-		sentry.CaptureException(err)
-		panic(err)
+		return 0, err
 	}
 
 	o, err := doc.DataAt("offset")
 	if err != nil {
-		fmt.Println("failed to fetch offset value from document")
-		sentry.CaptureException(err)
-		panic(err)
+		return 0, err
 	}
 
-	offset := o.(int64)
+	offset, ok := o.(int64)
+	if !ok {
+		return 0, err
+	}
 
-	fmt.Println("offset", offset)
-	return offset
+	return offset, nil
 }
 
-func getCurrentOffset(ctx context.Context, url string) int {
+func getNextOffset(ctx context.Context, url string) (int, error) {
 	h, err := http.Head(url)
 	if err != nil {
-		sentry.CaptureException(err)
-		panic(err)
+		return 0, err
 	}
 	defer h.Body.Close()
 
 	clen := h.Header["Content-Length"][0]
 	len, err := strconv.Atoi(clen)
 	if err != nil {
-		sentry.CaptureException(err)
-		panic(err)
+		return 0, nil
 	}
 
-	return len
+	return len, nil
 }
 
-func processHistoricalStateRow(row []string) historicalRow {
+func processHistoricalStateRow(row []string) (historicalRow, error) {
+	if len(row) != 6 {
+		return historicalRow{}, fmt.Errorf("csv row length mismatch; bad offset")
+	}
+
 	return historicalRow{
 		Date:   row[0],
 		State:  row[1],
 		Fips:   row[2],
 		Cases:  row[3],
 		Deaths: row[4],
-	}
+	}, nil
 }
 
-func processHistoricalCountyRow(row []string) historicalRow {
+func processHistoricalCountyRow(row []string) (historicalRow, error) {
+	if len(row) != 6 {
+		return historicalRow{}, fmt.Errorf("csv row length mismatch; bad offset")
+	}
+
 	return historicalRow{
 		Date:   row[0],
 		County: row[1],
@@ -178,5 +215,5 @@ func processHistoricalCountyRow(row []string) historicalRow {
 		Fips:   row[3],
 		Cases:  row[4],
 		Deaths: row[5],
-	}
+	}, nil
 }
